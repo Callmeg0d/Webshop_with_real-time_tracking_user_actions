6495704a60767e92e444d0f00bd3464b71cccc4f
Egor Muratov <egor.muratov.27@mail.ru>
2025-11-03 20:20:33 +0300

    Добавлены сущность и mapper для корзины

---
+++ app/domain/entities/cart.py
@@ -0,0 +1,10 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class CartItem:
+    cart_id: int | None  # None при создании, int после сохранения в БД
+    user_id: int
+    product_id: int
+    quantity: int
+    total_cost: int
---
+++ app/domain/mappers/cart.py
@@ -0,0 +1,31 @@
+from app.domain.entities.cart import CartItem
+from app.models import ShoppingCarts
+
+class CartMapper:
+    @staticmethod
+    def to_entity(orm_model: ShoppingCarts) -> CartItem:
+        return CartItem(
+            cart_id=orm_model.cart_id,
+            user_id=orm_model.user_id,
+            product_id=orm_model.product_id,
+            quantity=orm_model.quantity,
+            total_cost=orm_model.total_cost
+        )
+
+    @staticmethod
+    def to_orm(entity: CartItem) -> dict:
+        return {
+            # cart_id не включаем - БД сама присвоит при создании
+            "user_id": entity.user_id,
+            "product_id": entity.product_id,
+            "quantity": entity.quantity,
+            "total_cost": entity.total_cost
+        }
+
+    @staticmethod
+    def get_id(entity: CartItem) -> int | None:
+        return entity.cart_id
+
+    @staticmethod
+    def get_id_column_name() -> str:
+        return "cart_id"
