e2b1278be41430f04fc36a0e416ec51fbf58df44
Egor Muratov <egor.muratov.27@mail.ru>
2025-11-10 19:58:07 +0300

    Добавлены докстринги к сущностям, атрибуты со значением по умолчанию перенесён после всех

--- app/domain/entities/cart.py
+++ app/domain/entities/cart.py
@@ -3,6 +3,7 @@ from dataclasses import dataclass
 
 @dataclass
 class CartItem:
+    """Domain entity для корзины товаров."""
     cart_id: int | None  # None при создании, int после сохранения в БД
     user_id: int
     product_id: int
--- app/domain/entities/orders.py
+++ app/domain/entities/orders.py
@@ -5,10 +5,11 @@ from typing import List, Dict
 
 @dataclass
 class OrderItem:
-    order_id: int | None  # None при создании, int после сохранения в БД
+    """Domain entity для заказов."""
     user_id: int
     created_at: date
     status: str
     delivery_address: str
     order_items: List[Dict[str, str | int]]
     total_cost: int
+    order_id: int | None = None  # None при создании, int после сохранения в БД
